f8f559c30ea0b889e2ea885848a2ac9447657545
Raghib <noor.raghib.12@gmail.com>
2024-01-19 01:39:49 +0600

    test_18

--- birthday_bot/models.py
+++ birthday_bot/models.py
@@ -4,13 +4,18 @@ from pgvector.django import VectorField
 # Create your models here.
 
 
-class UserQuery(models.Model):
+
+class Events(models.Model):
     event_date=models.DateField()
+    english_text=models.TextField()
+    bengali_text=models.TextField()
+    vector=VectorField(dimensions=1536)
+
+class UserQuery(models.Model):
     user= models.ForeignKey(User,on_delete=models.CASCADE)
     relation= models.CharField(max_length=200)
     question=models.TextField()
     answer=models.CharField()
-    vectors=VectorField(dimensions=1536)
     
     def __repr__(self):
         return self.user.email
--- birthday_bot/serializer.py
+++ birthday_bot/serializer.py
@@ -1,7 +1,17 @@
-from rest_framework import serializers
-from .models import *
-from django.db.models import Q
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import uuid
+from .manager import UserManager 
+import datetime
 from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth.models import auth
 
 
+
+
+
+
+class UploadSerializer(serializers.Serializer):
+    file=serializers.FileField()
